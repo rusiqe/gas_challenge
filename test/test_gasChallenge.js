59dfd6a95fd43216053a72d65b34080299e6b99c
Taurai. M <rusiqe@gmail.com>
2023-07-14 13:45:30 +0200

    complete test

--- test/test_gasChallenge.js
+++ test/test_gasChallenge.js
@@ -20,7 +20,8 @@ describe("Deploy Gas Challenge Contract", () => {
 
   describe("Check Sum Of Array", () => {
     it("Should return 0", async () => {
-      // Write test block here to check sum of array equals 0
+      const sum = await gas_contract.getSumOfArray();
+      expect(sum).to.equal(0);
     });
   });
 });
